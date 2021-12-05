7c26745158a2ddcd51a0f75717e5eb7529c750e9
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-05 20:47:30 +0300

    add & get tasks functions

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import React, { useState, useEffect } from "react";
 
 const List = () => {
-  return (
-    <div>
-      list
-    </div>
-  )
-}
+  const [token, setToken] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  const getAllTasks = async (token) => {
+    try {
+      const list = await axios.get(`${process.env.REACT_APP_BASE_URL}/todos`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setTodos(list.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addTask = async () => {
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/task`,
+        {
+          task,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return <div>list</div>;
+};
 
 export default List;
-
-
